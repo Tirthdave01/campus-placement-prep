aef2bbd08e7367bdd80a699c54653d0c8390a609
Tirth Dave <24012011205@gnu.ac.in>
2026-09-14 13:15:19 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,10 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
-    ' http://localhost:5176',
-    process.env.FRONTEND_URL,
+    'http://localhost:5176',
+    'https://campus-placement-prep.vercel.app'
   ],
+  credentials: true
 }));
 
 app.use(express.json());
